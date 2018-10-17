15ec5ff403c9fefed1d19b40d57e76e9fcf57986
chc5 <chc9511@gmail.com>
2018-10-17 15:13:11 -0400

    express setup

---
+++ server/server.js
@@ -0,0 +1,8 @@
+// import express from 'express';
+const express = require('express');
+
+const app = express();
+const port = 4567;
+
+app.get('/api', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
