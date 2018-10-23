37901b2fd26a9870bb5d60a71289f92c23a391d8
Gerry Xu <gerryxu.zqx@gmail.com>
2018-10-23 17:31:06 -0400

    Changed and added more styling to UserInputFoodChoices component

--- src/User/UserHome/UserInputFoodChoices/UserInputFoodChoices.js
+++ src/User/UserHome/UserInputFoodChoices/UserInputFoodChoices.js
@@ -1,194 +1,211 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Button,
-    Card,
-    CardContent,
-    CardHeader,
-    TextField,
-    Snackbar,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  TextField,
+  Snackbar,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+  Chip
 } from "@material-ui/core";
-import "./UserInputFoodChoices.css"
+import "./UserInputFoodChoices.css";
 import { firebase } from "../../../Config";
 
 class UserInputFoodChoices extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputFoodName: '',
-            foodNames: [],
-            randomFoodName: '',
-            processing: false,
-            notify: false,
-            notifyMsg: '',
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleRandomFood = this.handleRandomFood.bind(this);
-    }
-    componentDidMount() {
-        this.fetchInitialData();
-    }
-    fetchInitialData() {
-        firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                firebase.database().ref(`Users/${user.uid}/foodNames`).once("value", (snapshot) => {
-                    if (snapshot.val() != null) {
-                        this.setState({
-                            foodNames: this.state.foodNames.concat(snapshot.val())
-                        })
-                    }
-                }, (error) => {
-                    console.log("Error: " + error.code);
-                })
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputFoodName: "",
+      foodNames: [],
+      randomFoodName: "",
+      processing: false,
+      notify: false,
+      notifyMsg: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleRandomFood = this.handleRandomFood.bind(this);
+  }
+  componentDidMount() {
+    this.fetchInitialData();
+  }
+  fetchInitialData() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        firebase
+          .database()
+          .ref(`Users/${user.uid}/foodNames`)
+          .once(
+            "value",
+            snapshot => {
+              if (snapshot.val() != null) {
+                this.setState({
+                  foodNames: this.state.foodNames.concat(snapshot.val())
+                });
+              }
+            },
+            error => {
+              console.log("Error: " + error.code);
             }
+          );
+      }
+    });
+  }
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.inputFoodName) {
+      this.setState({
+        foodNames: this.state.foodNames.concat(this.state.inputFoodName),
+        inputFoodName: "",
+        processing: true
+      });
+      const foodNamesRef = firebase
+        .database()
+        .ref(`Users/${firebase.auth().currentUser.uid}/foodNames`);
+      foodNamesRef
+        .set(this.state.foodNames.concat(this.state.inputFoodName))
+        .then(() => {
+          this.setState({ processing: false });
+        })
+        .catch(error => {
+          this.setState({
+            notify: true,
+            notifyMsg: error.message,
+            processing: false
+          });
         });
     }
-    handleInputChange(e) {
+  }
+  handleRandomFood(e) {
+    e.preventDefault();
+    const rand = this.state.foodNames[
+      Math.floor(Math.random() * this.state.foodNames.length)
+    ];
+    this.setState({
+      randomFoodName: rand,
+      processing: true
+    });
+    const foodSelectedRef = firebase
+      .database()
+      .ref(`Users/${firebase.auth().currentUser.uid}/foodSelected`);
+    foodSelectedRef
+      .set(rand)
+      .then(() => {
+        this.setState({ processing: false });
+      })
+      .catch(error => {
         this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        if (this.state.inputFoodName) {
-            this.setState({
-                foodNames: this.state.foodNames.concat(this.state.inputFoodName),
-                inputFoodName: '',
-                processing: true
-            });
-            const foodNamesRef = firebase.database().ref(`Users/${firebase.auth().currentUser.uid}/foodNames`)
-            foodNamesRef.set(this.state.foodNames.concat(this.state.inputFoodName))
-                .then(() => {
-                    this.setState({ processing: false })
-                })
-                .catch((error) => {
-                    this.setState({
-                        notify: true,
-                        notifyMsg: error.message,
-                        processing: false,
-                    });
-                })
-        }
-    }
-    handleRandomFood(e) {
-        e.preventDefault();
-        const rand = this.state.foodNames[Math.floor(Math.random() * this.state.foodNames.length)];
-        this.setState({
-            randomFoodName: rand,
-            processing: true
-        })
-        const foodSelectedRef = firebase.database().ref(`Users/${firebase.auth().currentUser.uid}/foodSelected`)
-        foodSelectedRef.set(rand)
-            .then(() => {
-                this.setState({ processing: false })
-            })
-            .catch((error) => {
-                this.setState({
-                    notify: true,
-                    notifyMsg: error.message,
-                    processing: false,
-                });
-            })
-    }
-    render() {
-        return (
-            <Card className="input-paper">
-                <CardHeader
-                    title="Please enter your food choices:"
-                />
-                <CardContent >
-                    <Snackbar
-                        onClose={() => {
-                            this.setState({ notify: false, notifyMsg: "" });
-                        }}
-                        open={this.state.notify}
-                        autoHideDuration={6000}
-                        message={this.state.notifyMsg}
-                    />
-                    <TextField
-                        fullWidth
-                        inputProps={{
-                            style: { textAlign: "center" }
-                        }}
-                        value={this.state.inputFoodName}
-                        className="addFoodChoice"
-                        onChange={this.handleInputChange}
-                        title="Add your favorite foods"
-                        name="inputFoodName"
-                        required
-                    />
-                    <Button
-                        style={{ marginTop: 50, marginBottom: 5, marginLeft: 20, marginRight: 20 }}
-                        variant="raised"
-                        color="primary"
-                        className="input-button"
-                        onClick={this.handleSubmit}
-                        disabled={!this.state.inputFoodName}
-                    >
-                        Add Food
+          notify: true,
+          notifyMsg: error.message,
+          processing: false
+        });
+      });
+  }
+  render() {
+    return (
+      <Card className="input-paper">
+        <CardHeader title="Please enter your food choices:" />
+        <CardContent>
+          <Snackbar
+            onClose={() => {
+              this.setState({ notify: false, notifyMsg: "" });
+            }}
+            open={this.state.notify}
+            autoHideDuration={6000}
+            message={this.state.notifyMsg}
+          />
+          <TextField
+            fullWidth
+            inputProps={{
+              style: { textAlign: "center" }
+            }}
+            value={this.state.inputFoodName}
+            className="addFoodChoice"
+            onChange={this.handleInputChange}
+            placeholder="Food name"
+            name="inputFoodName"
+            required
+          />
+          <Button
+            style={{
+              marginTop: 30,
+              marginBottom: 15
+            }}
+            variant="raised"
+            color="primary"
+            className="input-button"
+            onClick={this.handleSubmit}
+            disabled={!this.state.inputFoodName}
+          >
+            Add Food
+          </Button>
+          {this.state.foodNames.length > 0 ? (
+            <Paper>
+              <Table style={{ textAlign: "center" }}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Food Name</TableCell>
+                    <TableCell>Edit</TableCell>
+                    <TableCell>Delete</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.foodNames.map(food => (
+                    <TableRow key={food}>
+                      <TableCell component="th" scope="row">
+                        {food}
+                      </TableCell>
+                      <TableCell>
+                        <Button className="table-button" color="primary">
+                          Edit
                         </Button>
-                    {this.state.foodNames.length > 0 ? (
-                        <Paper>
-                            <Table
-                                style={{textAlign: 'center', }}
-                            >
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Food Name</TableCell>
-                                        <TableCell>Edit</TableCell>
-                                        <TableCell>Delete</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {this.state.foodNames.map((food) => (
-                                        <TableRow key={food}>
-                                            <TableCell>{food}</TableCell>
-                                            <TableCell>
-                                                <Button
-                                                    color="primary"
-                                                >
-                                                    Edit
-                                                </Button>
-                                            </TableCell>
-                                            <TableCell>
-                                            <Button
-                                                    color="secondary"
-                                                >
-                                                    Delete
-                                                </Button>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-
-                            <Button
-                                style={{ marginTop: 50, marginBottom: 5, marginLeft: 20, marginRight: 20, }}
-                                variant="raised"
-                                color="secondary"
-                                className="input-button"
-                                onClick={this.handleRandomFood}
-                            >
-                                Generate Random Food
-                            </Button>
-                        </Paper>
-                    ) : null}
-                    {this.state.randomFoodName !== '' ? (
-                        <div>
-                            Your Food is: {this.state.randomFoodName}
-                        </div>
-                    ) : null}
-                </CardContent>
-            </Card>
-        )
-    }
+                      </TableCell>
+                      <TableCell>
+                        <Button className="table-button" color="secondary">
+                          Delete
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Paper>
+          ) : null}
+          <Button
+            style={{
+              marginTop: 10,
+              marginBottom: 5
+            }}
+            variant="raised"
+            color="secondary"
+            className="input-button"
+            onClick={this.handleRandomFood}
+          >
+            Generate Random Food
+          </Button>
+          <div className="random-food-section">
+            {this.state.randomFoodName !== "" ? (
+              <Typography variant="subtitle1">
+                The food selected is: <Chip label={this.state.randomFoodName} />
+              </Typography>
+            ) : null}
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
 }
-
 
 export default UserInputFoodChoices;
