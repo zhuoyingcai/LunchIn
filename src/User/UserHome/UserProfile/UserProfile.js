438532ef729206014e4288fbfe8357698d372332
Zhuoying Cai <zhuoyingcai97@gmail.com>
2018-10-29 10:58:07 -0400

    user profile page

--- src/User/UserHome/UserProfile/UserProfile.js
+++ src/User/UserHome/UserProfile/UserProfile.js
@@ -1,87 +1,163 @@
 import React, { Component } from 'react';
+import "./UserProfile.css";
 import { firebase } from "../../../Config";
 import {
-    Button,
-    Card,
-    CardContent,
-    CardHeader,
-    Divider,
-    IconButton,
-    InputAdornment,
-    Snackbar,
-    TextField,
-    Typography
-  } from "@material-ui/core";
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Snackbar,
+  TextField,
+  Typography
+} from "@material-ui/core";
 import Back from "@material-ui/icons/ArrowBack";
 
-
-class UserProfile extends Component { 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "",
-            email: "",
-            password: "",
-            address: "",
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      address: "",
+      processing: false,
+      notify: false,
+      notifyMsg: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    this.fetchInitialData();
+  }
+  fetchInitialData() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        firebase
+          .database()
+          .ref(`Users/${user.uid}`)
+          .once(
+            "value",
+            snapshot => {
+              if (snapshot.val() != null) {
+                this.setState({
+                  name: snapshot.val().name,
+                  address: snapshot.val().address
+                });
+              }
+            },
+            error => {
+              console.log("Error: " + error.code);
+            }
+          );
+      }
+    });
+  }
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.address) {
+      this.setState({
+        address: this.state.address,
+        processing: true
+      });
+      const addressRef = firebase
+        .database()
+        .ref(`Users/${firebase.auth().currentUser.uid}/address`);
+      addressRef
+        .set(this.state.address)
+        .then(() => {
+          this.setState({
             processing: false,
-            notify: false,
-            notifyMsg: ""
-        };
+            notify: true,
+            notifyMsg: "Update successfully.",
+          });
+        })
+        .catch(error => {
+          this.setState({
+            notify: true,
+            notifyMsg: error.message,
+            processing: false
+          });
+        });
     }
+  }
 
-    render() {
-        return(
-            <div style={{ padding: "50px 200px" }}>
-            {/*=============WELCOME USER HEADER=============*/}
-                <div className="user-header">
+  render() {
+    return (
+      <div style={{ padding: "50px 200px" }}>
+        {/*=============WELCOME USER HEADER=============*/}
+        <div className="user-header">
           <Typography variant="display2" style={{ flex: 1 }}>
-          Profile
+            Profile
 
-          <Button
+        <Button
               style={{ float: "right" }}
               onClick={() => {
                 this.props.history.push(`/user/home`);
               }}>
-            <Back />Back
-          </Button>
+              <Back />Back
+        </Button>
 
           </Typography>
-          </div>
-          <Divider />
+        </div>
+        <Divider />
 
-        <div className="signup-page-content">
-          <Card style={{ marginTop: "10px" }} data-aos="slide-up">
-            <CardContent>
-
-            <TextField
-                id="outlined-required"
-                label="Name"
-                //className={classes.textField}
-                value="joy"
-                //onChange={this.handleChange('name')}
-                fullWidth
-                margin="normal"
-                variant="outlined"
-            />
-
-                    <TextField
-          id="outlined-email-input"
-          label="Email"
-          //className={classes.textField}
-          type="email"
-          name="email"
-          autoComplete="email"
-          margin="normal"
-          variant="outlined"
-          fullWidth
+        <Snackbar
+          onClose={() => {
+            this.setState({ notify: false, notifyMsg: "" });
+          }}
+          open={this.state.notify}
+          autoHideDuration={6000}
+          message={this.state.notifyMsg}
         />
-   
+
+        <div className="user-profile-content">
+          <Card className="input-paper" data-aos="zoom-in-up">
+            <CardContent>
+              <TextField
+                id="name"
+                label="Full Name"
+                value={this.state.name}
+                fullWidth
+                className="push-down"
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+
+              <TextField
+                onChange={this.handleChange("address")}
+                id="address"
+                label="Full Address"
+                required
+                value={this.state.address}
+                fullWidth
+                className="push-down"
+                disabled={this.state.processing}
+              />
+
+              <Button
+                style={{
+                  float: "right",
+                  marginTop: 10,
+                }}
+                variant="raised"
+                color="primary"
+                className="input-button"
+                onClick={this.handleSubmit}
+              >
+                Update
+              </Button>
             </CardContent>
           </Card>
-
-          </div>
-          </div>
-        )
-    }
+        </div>
+      </div>
+    )
+  }
 }
 export default UserProfile;
