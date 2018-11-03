98be84f669397d931482e03ac30d9f1e1789e25e
chc5 <chc9511@gmail.com>
2018-11-03 15:13:44 -0400

    Stored Yelp's API keys in local env.

--- server/controllers/yelpApi.js
+++ server/controllers/yelpApi.js
@@ -1,13 +1,13 @@
 const yelp = require('yelp-fusion');
-const clientID = 'H_7NXPInp_41ZuEroRrm0w';
-const yelpApiKey = 'whkFooEOGksI2QwFSlH384Ms_QthKYxnr6mhtM2KQdbKyh5AroW28JH_bS0N7Jzk3xhl0qwD4v61vnSiyrqAqvwxqjh_A-QovSzYq-bjDKyo3j5QhyMQXh7sEPzAW3Yx';
+const clientID = process.env.YELP_CLIENT_ID;
+const yelpApiKey = process.env.YELP_API_KEY;
 
 /**
 	Soon to be implemented:
 
 **/
 function hasValidYelpParams(query, res){
-	if(!query.term || !query.location){
+ if(!query.term || !query.location){
 		res.status(400).json({ errorMsg: 'Required Yelp parameters are not found.'});
 		return false;
 	}
