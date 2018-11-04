3f5f82b9b94b33afe406c4c1de22efe45138971a
chc5 <chc9511@gmail.com>
2018-11-04 16:46:01 -0500

    Added unit tests to Home and Signup

---
+++ src/Home/Home.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import Home from "./Home";
+
+describe("Home Component", () => {
+  it("Should render correctly with correct path", () => {
+    const HomeInstance = shallow(
+      <Home />
+    );
+    expect(HomeInstance).toMatchSnapshot();
+  });
+});
