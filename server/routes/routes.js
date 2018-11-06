803bd2b2eb62a97233d85d6c502060cec658e2cc
chc5 <chc9511@gmail.com>
2018-11-06 00:10:22 -0500

    Removed getApiKeys and unit tests because of redundancy.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const yelpApi = require('../controllers/yelpApi');
-const apiKeys = require('../controllers/apiKeys');
 
 router.get('/api/yelp', yelpApi.getApi);
 router.post('/api/yelp', yelpApi.postApi);
-
-router.post('/apiKey', apiKeys.postApiKey);
 
 router.get('/', (req, res) => res.status(200).send('Express Router is responding...'));
 
