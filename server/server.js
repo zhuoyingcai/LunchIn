e2c108d33cd2b00d98797b86ceb864c6bf883ca1
chc5 <chc9511@gmail.com>
2018-11-06 20:53:25 -0500

    Added cors for production

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const router = require('./routes/routes');
 const bodyParser = require('body-parser');
 
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const server_port = 5000;
 const cilent_port = 3000;
@@ -13,7 +13,7 @@ const cilent_port = 3000;
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(cors());
 app.use(router);
 
 var server = app.listen(server_port, () => console.log(`Express app listening on port ${server_port}!`));
