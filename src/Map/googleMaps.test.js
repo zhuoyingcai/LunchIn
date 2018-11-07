d3f31e28f97e79475c34ba45c5c81f0216ff0fac
Luis Bueno <buenoluis007@gmail.com>
2018-11-07 11:50:37 -0500

    Updated jest snapshot test

--- src/Map/googleMaps.test.js
+++ src/Map/googleMaps.test.js
@@ -16,10 +16,14 @@ describe("Maps Component", () => {
     it("Should render correctly", () => {
       const food = "Pizza";
       const address = "160 Convent Ave, New York, NY 10031"
+      const lat = 40.8216144
+      const lng = -73.9479427
       const mapInstance = shallow(
         <GoogleM 
             food={food} 
             address={address}
+            lat={lat}
+            lng={lng}
         />
       );
       expect(mapInstance).toMatchSnapshot();
