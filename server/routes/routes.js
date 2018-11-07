1d3097b422255461980079f59efd9e4ef073f458
chc5 <chc9511@gmail.com>
2018-11-06 22:18:28 -0500

    Production build

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 const yelpApi = require('../controllers/yelpApi');
 
 router.get('/api/yelp', yelpApi.getApi);
 router.post('/api/yelp', yelpApi.postApi);
 
-router.get('/', (req, res) => res.status(200).send('Express Router is responding...'));
+// router.get('/', (req, res) => res.status(200).send('Express Router is responding...'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  // Handle React routing, return all requests to React app
+  router.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../../build/index.html'));
+  });
+}
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
+// process.env.NODE_ENV = 'production';
 require('dotenv').load();
 // console.log(process.env);
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes/routes');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const cors = require('cors');
 const app = express();
-const server_port = 5000;
+const server_port = process.env.PORT || 5000;
 const cilent_port = 3000;
 
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+// Serve any static files
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname + '/../build')));
+}
 app.use(router);
 
 var server = app.listen(server_port, () => console.log(`Express app listening on port ${server_port}!`));
