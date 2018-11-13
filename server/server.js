e308b3e4961538373f490c132d9fc3d2c83659d9
chc5 <chc9511@gmail.com>
2018-11-12 23:47:32 -0500

    Removed unnecessary logging and renamed several variables

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require('dotenv').load();
-console.log(process.env);
+
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes/routes');
@@ -7,8 +7,7 @@ const bodyParser = require('body-parser');
 
 const cors = require('cors');
 const app = express();
-const server_port = 5000;
-const cilent_port = 3000;
+const port = process.env.PORT || 5000;
 
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,6 +15,6 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(router);
 
-var server = app.listen(server_port, () => console.log(`Express app listening on port ${server_port}!`));
+var server = app.listen(port, () => console.log(`Express app listening on port ${port}!`));
 
 module.exports = server;
