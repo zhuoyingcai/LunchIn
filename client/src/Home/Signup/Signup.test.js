ad0361e9ac6c7cc8c506928bc235613ce94b55f3
Zhuoying Cai <zhuoyingcai97@gmail.com>
2018-11-20 17:04:03 -0500

    fix test

--- client/src/Home/Signup/Signup.test.js
+++ client/src/Home/Signup/Signup.test.js
@@ -8,6 +8,29 @@ describe("Signup Component", () => {
     const SignupInstance = shallow(<Signup />);
     expect(SignupInstance).toMatchSnapshot();
   });
+
+  describe("When provided with missing user info", () => {
+    it("Should render correctly", () => {
+      const name = "Test";
+      const email = " "
+      const address = "160 Convent Ave, New York, NY 10031"
+      const password = "password"
+      const lat = 40.8216144
+      const lng = -73.9479427
+      const SignupInstance = shallow(
+        <Signup 
+            name={name} 
+            email={email}
+            address={address}
+            password={password}
+            lat={lat}
+            lng={lng}
+        />
+      );
+      expect(SignupInstance).toMatchSnapshot();
+    });
+  });
+
   describe("When provided with user info", () => {
     it("Should render correctly", () => {
       const name = "Test";
@@ -29,6 +52,7 @@ describe("Signup Component", () => {
       expect(SignupInstance).toMatchSnapshot();
     });
   });
+
   describe("When provided with empty address info", () => {
     it("Should render correctly", () => {
       const name = "Test";
@@ -46,6 +70,7 @@ describe("Signup Component", () => {
       expect(SignupInstance).toMatchSnapshot();
     });
   });
+
   describe("When provided with invalid address info", () => {
     it("Should render correctly", () => {
       const name = "Test";
