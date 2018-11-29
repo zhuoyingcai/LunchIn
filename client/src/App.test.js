cc9f0c0fd002c45cbb299382ba93e94d39b5c4c5
Gerry Xu <gerryxu.zqx@gmail.com>
2018-11-29 18:50:11 -0500

    Updated App test to suppose enzyme-to-json to ensure that we get more focused test, such that only the component own markup get serialized into the snapshot

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,11 +4,28 @@ import App from "./App";
 import { mount, shallow } from "enzyme";
 import Home from "./Home/Home.js";
 import Signup from "./Home/Signup/Signup.js";
+import toJson, {
+  shallowToJson,
+  mountToJson,
+  renderToJson
+} from "enzyme-to-json";
 
 describe("App Component", () => {
   it("should renders without crashing", () => {
     const wrapper = shallow(<App />);
-    expect(wrapper).toMatchSnapshot();
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+  it("converts shallow wrapper correctly", () => {
+    const wrapper = shallow(<App />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it("converts mount wrapper correctly", () => {
+    const wrapper = shallow(<App />);
+    expect(mountToJson(wrapper)).toMatchSnapshot();
+  });
+  it("converts render wrapper correctly", () => {
+    const wrapper = shallow(<App />);
+    expect(renderToJson(wrapper)).toMatchSnapshot();
   });
 });
 
