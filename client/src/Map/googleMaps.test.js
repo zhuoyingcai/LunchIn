96543467239e2be48e1b62c310dac2b0b79cb5c6
Evan Vasquez <32246279+EvanVasquez@users.noreply.github.com>
2018-12-02 21:27:32 -0500

    updated test cases

--- client/src/Map/googleMaps.test.js
+++ client/src/Map/googleMaps.test.js
@@ -18,12 +18,16 @@ describe("Maps Component", () => {
       const address = "160 Convent Ave, New York, NY 10031"
       const lat = 40.8216144
       const lng = -73.9479427
+      const storeN = "Uncle Tony's Pizza"
+      const storeL = "1596 Amsterdam Ave, New York"
       const mapInstance = shallow(
-        <GoogleM 
-            food={food} 
+        <GoogleM
+            food={food}
             address={address}
             lat={lat}
             lng={lng}
+            storeN={storeN}
+            storeL={storeL}
         />
       );
       expect(mapInstance).toMatchSnapshot();
