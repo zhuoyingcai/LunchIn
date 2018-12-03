c5271c02040a7ff6135933b8b9b43ae9c8eedd78
Evan Vasquez <32246279+EvanVasquez@users.noreply.github.com>
2018-12-02 23:19:11 -0500

    New test case for gmap
    added a new test case for google maps specifically for only lng and lat inputs

--- client/src/Map/googleMaps.test.js
+++ client/src/Map/googleMaps.test.js
@@ -12,6 +12,8 @@ describe("Maps Component", () => {
       expect(mapInstance).toMatchSnapshot();
     });
   });
+
+
   describe("When provided with food and address info", () => {
     it("Should render correctly", () => {
       const food = "Pizza";
@@ -33,4 +35,22 @@ describe("Maps Component", () => {
       expect(mapInstance).toMatchSnapshot();
     });
   });
+
+  describe("When provided only lat and lng and address", () => {
+    it("Should render correctly", () => {
+      const address = "160 Convent Ave, New York, NY 10031"
+      const lat = 40.8216144
+      const lng = -73.9479427
+      const mapInstance = shallow(
+        <GoogleM
+            address={address}
+            lat={lat}
+            lng={lng}
+        />
+      );
+      expect(mapInstance).toMatchSnapshot();
+    });
+  });
+
+
 });
