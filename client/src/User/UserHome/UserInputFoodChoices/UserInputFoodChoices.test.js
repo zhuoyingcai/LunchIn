9f94a426d90923389e16b5922d517b19ae77f699
sengfung27 <sengfung27@hotmail.com>
2018-12-04 00:38:15 -0500

    firebase mock tests in UserInputFoodChoices component

--- client/src/User/UserHome/UserInputFoodChoices/UserInputFoodChoices.test.js
+++ client/src/User/UserHome/UserInputFoodChoices/UserInputFoodChoices.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UserInputFoodChoices from './UserInputFoodChoices';
 import { shallow } from 'enzyme';
 import toJSON, { shallowToJson, mountToJson, renderToJson } from 'enzyme-to-json';
+import { firebase } from "../../../Config";
 
 describe('UserInputFoodChoices Component', () => {
     it('renders UserInputFoodChoices Component correctly', () => {
@@ -13,7 +14,7 @@ describe('UserInputFoodChoices Component', () => {
         const wrapper = shallow(<UserInputFoodChoices />)
         expect(shallowToJson(wrapper)).toMatchSnapshot();
     })
-    
+
     it('converts mount wrapper correctly', () => {
         const wrapper = shallow(<UserInputFoodChoices />)
         expect(mountToJson(wrapper)).toMatchSnapshot();
@@ -26,17 +27,78 @@ describe('UserInputFoodChoices Component', () => {
 
     it('sanitize the input before passing as props. Test 1', () => {
         const wrapper = shallow(<UserInputFoodChoices />)
-        expect(wrapper.instance().sanitizeInput('\'')).toBe('&#x27;')      
+        expect(wrapper.instance().sanitizeInput('\'')).toBe('&#x27;')
     })
 
-    it('sanitizes the inputs before passing as props. Test 2', () =>{
+    it('sanitizes the inputs before passing as props. Test 2', () => {
         const wrapper = shallow(<UserInputFoodChoices />)
         expect(wrapper.instance().sanitizeInput('"')).toBe('&quot;')
     })
 
-    it('should render UserInputFoodChoices with inputs food.', () =>{
-        const wrapper = shallow(<UserInputFoodChoices inputFoodName={"halal"}/>)   
+    it('should render UserInputFoodChoices with inputs food.', () => {
+        const wrapper = shallow(<UserInputFoodChoices inputFoodName={"halal"} />)
         expect(toJSON(wrapper)).toMatchSnapshot();
     })
+
+    it("Should add the food into the food list ", async done => {
+        const spy = jest.spyOn(firebase, "auth");
+        const wrapper = shallow(<UserInputFoodChoices />);
+        const instance = wrapper.instance();
+        instance.setState({
+            inputFoodName: 'halal'
+        });
+        await firebase
+            .database()
+            .ref()
+            .child("users")
+            .orderByChild("foodNames")
+            .limitToFirst(1)
+            .on("value", function (snapshot) {
+                expect(snapshot.exists());
+            });
+        expect(spy).toHaveBeenCalled();
+        spy.mockRestore();
+        done();
+    });
+
+    it("Should generate random food from the food list", async done => {
+        const spy = jest.spyOn(firebase, "auth");
+        const wrapper = shallow(<UserInputFoodChoices />);
+        const instance = wrapper.instance();
+        instance.setState({
+            randomFoodName: 'halal'
+        });
+        await firebase
+            .database()
+            .ref()
+            .child("users")
+            .orderByChild("foodSelected")
+            .on("value", function (snapshot) {
+                expect(snapshot.exists());
+            });
+        expect(spy).toHaveBeenCalled();
+        spy.mockRestore();
+        done();
+    });
+
+    it("Should generate sanitized random food from the food list", async done => {
+        const spy = jest.spyOn(firebase, "auth");
+        const wrapper = shallow(<UserInputFoodChoices />);
+        const instance = wrapper.instance();
+        instance.setState({
+            sanitizedRandomFood: 'halal'
+        });
+        await firebase
+            .database()
+            .ref()
+            .child("users")
+            .orderByChild("foodSelected")
+            .on("value", function (snapshot) {
+                expect(snapshot.exists());
+            });
+        expect(spy).toHaveBeenCalled();
+        spy.mockRestore();
+        done();
+    });
 
 });
